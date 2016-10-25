31577e764f33b4afbe3efcc4a4fdfb0f5329809f
spal7580 <psaineeraj@gmail.com>
2016-10-25 13:08:04 -0600

    creating Project CardGame

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Paladugu, Sai neeraj on 10/25/16.
+//  Copyright © 2016 Paladugu, Sai neeraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
