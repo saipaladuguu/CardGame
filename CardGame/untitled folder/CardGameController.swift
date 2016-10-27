decee15074e9637b0a90453cb2d4dd2ee0d47c3f
spal7580 <psaineeraj@gmail.com>
2016-10-27 12:30:24 -0600

    created the  cardgameContoller
    deleted the UI View and started the UI controller

---
+++ CardGame/untitled folder/CardGameController.swift
@@ -0,0 +1,14 @@
+//
+//  CardGameController.swift
+//  CardGame
+//
+//  Created by Paladugu, Sai neeraj on 10/27/16.
+//  Copyright © 2016 Paladugu, Sai neeraj. All rights reserved.
+//
+
+import UIKit
+
+class CardGameController : UIViewController
+{
+    
+}
